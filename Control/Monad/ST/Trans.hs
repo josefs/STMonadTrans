f212ce01920d7613a4a1c63ab141cd7da86aa824
Josef Svenningsson <josef.svenningsson@gmail.com>
2009-02-06 18:40:44 +0100

    Insert a warning text that this library should only be used with Affine Monads.
    darcs-hash:20090206174044-f316c-062d5c120f3b5fb1ec1f25c1212c6cfb3f64ee82

--- Control/Monad/ST/Trans.hs
+++ Control/Monad/ST/Trans.hs
@@ -12,6 +12,13 @@
 
    This library provides a monad transformer version of the ST monad.
 
+   Warning! This monad transformer should not be used with monads that
+   can contain multiple answers, like the list monad. The reason is that 
+   the will be duplicated across the different answers and this cause
+   Bad Things to happen (such as loss of referential transparency). Safe 
+   monads include the monads State, Reader, Writer, Maybe and 
+   combinations of their corresponding monad transformers.
+
 -}
 module Control.Monad.ST.Trans(
       -- * The ST Monad Transformer
