fabbc65dd158da0f7becfde7699a857956baefe0
Josef Svenningsson <josef.svenningsson@gmail.com>
2017-01-31 05:11:54 +0100

    Comments and layout

--- Control/Monad/ST/Trans.hs
+++ Control/Monad/ST/Trans.hs
@@ -14,10 +14,10 @@
 
    Warning! This monad transformer should not be used with monads that
    can contain multiple answers, like the list monad. The reason is that 
-   the will be duplicated across the different answers and this cause
-   Bad Things to happen (such as loss of referential transparency). Safe 
-   monads include the monads State, Reader, Writer, Maybe and 
-   combinations of their corresponding monad transformers.
+   the state token will be duplicated across the different answers and
+   this causes Bad Things to happen (such as loss of referential
+   transparency). Safe monads include the monads State, Reader, Writer,
+   Maybe and combinations of their corresponding monad transformers.
 
 -}
 module Control.Monad.ST.Trans(
